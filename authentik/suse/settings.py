946bb927209706f098bd35446efa69077dac6efe
Jose D. Gomez R. <jose.gomez@suse.com>
2026-05-12 17:39:15 +0200

    fixup: Redefine DRAMATIQ based on root.authentik.settings + suse extras
    And allow configuring more Dramatiq settings via CONFIG
    
    Keep the hardcoded strings as default values and allow overriding via
    the `CONFIG` object.

--- authentik/suse/settings.py
+++ authentik/suse/settings.py
@@ -1,25 +1,15 @@
 # Infrastructure specific settings
-from pathlib import Path
 
 from authentik.lib.config import CONFIG
-from authentik.lib.utils.time import timedelta_from_string
+from authentik.root.settings import DRAMATIQ as BASE_DRAMATIQ
 
 # S3 Configuration
 AWS_S3_OBJECT_PARAMETERS = CONFIG.get_dict_from_b64_json("suse.aws_s3_object_parameters", {})
 CONFIG.log("info", f"Loaded AWS_S3_OBJECT_PARAMETERS={AWS_S3_OBJECT_PARAMETERS}")
 
-# Not the most beautiful here, copying from authentik.root.settings. it's not
-# possible to import here the module because this module is called by
-# autentik.root.settings itself.
-
-# _update_settings("authentik.suse.settings")
-
-# That forces redefining some vars:
-
-BASE_DIR = Path(__file__).absolute().parent.parent.parent
-TEST = False
-
-
+# Dramatiq config
+#
+# Configure the Broker
 allowed_actors = CONFIG.get("worker.allowed_actors", "")
 ignored_actors = CONFIG.get("worker.ignored_actors", "")
 broker_class = CONFIG.get("worker.broker_class", "authentik.suse.worker.broker.Broker")
@@ -28,83 +18,42 @@ broker_kwargs = {
     "ignored_actors": [q.strip() for q in ignored_actors.split(",") if q.strip() != ""],
 }
 
-# Dramatiq config
-#
-# Adding our custom kwargs to the broker instead.
-DRAMATIQ = {
+# Configure the Scheduler
+schedule_model = CONFIG.get("worker.schedule_model", "authentik.tasks.schedules.models.Schedule")
+scheduler_class = CONFIG.get(
+    "worker.scheduler_class", "authentik.tasks.schedules.scheduler.Scheduler"
+)
+# Configure the results Backend
+result_backend = CONFIG.get("worker.result_backend", "authentik.suse.worker.results.Backend")
+
+# Insert the middleware next to LoggingMiddleware
+middleware_pivot = next(
+    (
+        idx
+        for idx, middleware_config in enumerate(BASE_DRAMATIQ["middlewares"])
+        if middleware_config[0] == "authentik.tasks.middleware.LoggingMiddleware"
+    )
+)
+
+middlewares = (
+    *(BASE_DRAMATIQ["middlewares"][:middleware_pivot]),
+    ("authentik.suse.worker.task_middlewares.AckWithBroker", {}),
+    *(BASE_DRAMATIQ["middlewares"][middleware_pivot:]),
+)
+
+# Define extras
+DRAMATIQ_EXTRAS = {
     "broker_class": broker_class,
     "broker_kwargs": broker_kwargs,
-    "channel_prefix": "authentik",
-    "task_model": "authentik.tasks.models.Task",
-    "task_purge_interval": timedelta_from_string(
-        CONFIG.get("worker.task_purge_interval")
-    ).total_seconds(),
-    "task_expiration": timedelta_from_string(CONFIG.get("worker.task_expiration")).total_seconds(),
-    "autodiscovery": {
-        "enabled": True,
-        "setup_module": "authentik.tasks.setup",
-        "apps_prefix": "authentik",
-    },
-    "worker": {
-        "processes": CONFIG.get_int("worker.processes", 2),
-        "threads": CONFIG.get_int("worker.threads", 1),
-        "consumer_listen_timeout": timedelta_from_string(
-            CONFIG.get("worker.consumer_listen_timeout")
-        ).total_seconds(),
-        "watch_folder": BASE_DIR / "authentik",
-    },
-    "result_backend": "authentik.suse.worker.results.Backend",
-    "scheduler_class": "authentik.tasks.schedules.scheduler.Scheduler",
-    "schedule_model": CONFIG.get(
-        "worker.schedule_model", "authentik.tasks.schedules.models.Schedule"
-    ),
-    "scheduler_interval": timedelta_from_string(
-        CONFIG.get("worker.scheduler_interval")
-    ).total_seconds(),
-    "middlewares": (
-        ("django_dramatiq_postgres.middleware.FullyQualifiedActorName", {}),
-        ("django_dramatiq_postgres.middleware.DbConnectionMiddleware", {}),
-        ("django_dramatiq_postgres.middleware.TaskStateBeforeMiddleware", {}),
-        ("dramatiq.middleware.age_limit.AgeLimit", {}),
-        (
-            "dramatiq.middleware.time_limit.TimeLimit",
-            {
-                "time_limit": timedelta_from_string(
-                    CONFIG.get("worker.task_default_time_limit")
-                ).total_seconds()
-                * 1000
-            },
-        ),
-        ("dramatiq.middleware.shutdown.ShutdownNotifications", {}),
-        ("dramatiq.middleware.callbacks.Callbacks", {}),
-        ("dramatiq.middleware.pipelines.Pipelines", {}),
-        (
-            "dramatiq.middleware.retries.Retries",
-            {
-                "max_retries": CONFIG.get_int("worker.task_max_retries") if not TEST else 0,
-                "max_backoff": 60 * 60 * 1000,  # 1 hour
-            },
-        ),
-        ("dramatiq.results.middleware.Results", {"store_results": True}),
-        ("authentik.tasks.middleware.StartupSignalsMiddleware", {}),
-        ("authentik.tasks.middleware.CurrentTask", {}),
-        ("authentik.tasks.middleware.TenantMiddleware", {}),
-        ("authentik.tasks.middleware.ModelDataMiddleware", {}),
-        ("authentik.tasks.middleware.TaskLogMiddleware", {}),
-        ("authentik.tasks.middleware.LoggingMiddleware", {}),
-        ("authentik.suse.worker.task_middlewares.AckWithBroker", {}),
-        ("authentik.tasks.middleware.DescriptionMiddleware", {}),
-        ("authentik.tasks.middleware.WorkerHealthcheckMiddleware", {}),
-        ("authentik.tasks.middleware.WorkerStatusMiddleware", {}),
-        (
-            "authentik.tasks.middleware.MetricsMiddleware",
-            {
-                "prefix": "authentik",
-            },
-        ),
-        ("django_dramatiq_postgres.middleware.TaskStateAfterMiddleware", {}),
-    ),
-    "test": TEST,
+    "result_backend": result_backend,
+    "scheduler_class": scheduler_class,
+    "schedule_model": schedule_model,
+    "middlewares": middlewares,
 }
+
+# Extend the default dramatiq configuration
+DRAMATIQ = {}
+DRAMATIQ.update(BASE_DRAMATIQ)
+DRAMATIQ.update(DRAMATIQ_EXTRAS)
 
 CONFIG.log("info", f"Loaded Dramatiq with settings DRAMATIQ={DRAMATIQ})")
