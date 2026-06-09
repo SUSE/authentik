e4b6a41a181598958e4a3aaac6537ead325eda59
Jose D. Gomez R. <jose.gomez@suse.com>
2026-06-09 16:12:46 +0200

    fix(test-runner): Respect error_reporting settings in test

--- authentik/root/test_runner.py
+++ authentik/root/test_runner.py
@@ -86,7 +86,9 @@ class PytestTestRunner(DiscoverRunner):  # pragma: no cover
         ASN_CONTEXT_PROCESSOR.load()
         GEOIP_CONTEXT_PROCESSOR.load()
 
-        sentry_init()
+        if CONFIG.get_bool("error_reporting.enabled", False):
+            sentry_init()
+
         self.logger.debug("Test environment configured")
 
         use_test_broker()
