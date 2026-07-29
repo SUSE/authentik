SUSE/authentik

commit 82a84f8e181fa1384c147ec13e6afd531ab50b22

fixup(sfe): Properly interpolate XML into HTML form values
`Stage#html(html)` method is an alias of `HTMLElement.html=html` which
expects a _properly_ formatted html input.

When the backend produces XML elements, `AutosubmitStage`
interpolates _as is_ into the `value` attribute of the form fields,
when a value contains a quote, it breaks the value boundary and the
rest of the content starts being interpreted as HTML (not as
the _literal_ value of the field).

Switched the input field generation logic from string interpolation to
jquery element builder [0] + .appendTo. It handles properly any
arbitrary value contents.

[0]: https://api.jquery.com/jquery/#jQuery2

web/packages/sfe/src/index.ts
@@ -252,19 +252,15 @@ class AutosubmitStage extends Stage<AutosubmitChallenge> {
             <form id="autosubmit-form" action="${this.challenge.url}" method="POST">
                 <img class="mb-4 brand-icon" src="${ak().brand.branding_logo}" alt="">
                 <h1 class="h3 mb-3 fw-normal text-center">${this.challenge?.flowInfo?.title}</h1>
-                ${Object.entries(this.challenge.attrs).map(([key, value]) => {
-                    return `<input
-                            type="hidden"
-                            name="${key}"
-                            value="${value}"
-                        />`;
-                })}
                 <div class="d-flex justify-content-center">
                     <div class="spinner-border" role="status">
                         <span class="sr-only">Loading...</span>
                     </div>
                 </div>
             </form>`);
+        Object.entries(this.challenge.attrs).map(([key, value]) => {
+            $("<input>", {name: key, type: "hidden"}).val(value).appendTo("#autosubmit-form")
+        })
         $("#autosubmit-form").submit();
     }
 }

commit bb120aae8c9b07359fabb226a969bb66a7f5e5e5

web/sfe: replace superfluous map in form values
Solves array-callback-return eslint failure. It appears .map() is
not needed here, as the return value is not used.

Fixes: 82a84f8e181f ("fixup(sfe): Properly interpolate XML into HTML form values")
Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

web/packages/sfe/src/index.ts
@@ -258,7 +258,7 @@ class AutosubmitStage extends Stage<AutosubmitChallenge> {
                     </div>
                 </div>
             </form>`);
-        Object.entries(this.challenge.attrs).map(([key, value]) => {
+        Object.entries(this.challenge.attrs).forEach(([key, value]) => {
             $("<input>", {name: key, type: "hidden"}).val(value).appendTo("#autosubmit-form")
         })
         $("#autosubmit-form").submit();