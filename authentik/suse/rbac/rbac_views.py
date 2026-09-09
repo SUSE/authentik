97a03c154c7d83daed22958dba6bbcbc736a25f1
Jose D. Gomez R. <jose.gomez@suse.com>
2026-09-09 13:18:10 +0200

    feat(suse,rbac): Improve serializer in RBAC role permission list
    The new serializer is not prone to N*M queries as the upstream one.
    
    It has the same object shape, the only difference is cosmetic as it
    doesn't de-reference the referenced target and permission, it rather
    names it in "code"-form.
    
    Permission name is returned as "app_label.codename on model#target-pk"
    instead of "Can view Group" or "Group #name# group".
    
    Everything is gathered in a single query with joins by django ORM.
    
    Pagination class is also dynamically changed so it follows the global
    tenant settings.

---
+++ authentik/suse/rbac/rbac_views.py
@@ -0,0 +1,71 @@
+from django.conf import settings
+from guardian.models import RoleObjectPermission
+from rest_framework.fields import CharField, IntegerField, SerializerMethodField
+
+from authentik.api.pagination import Pagination
+from authentik.core.api.utils import PassiveSerializer
+from authentik.rbac.api.rbac_roles import RolePermissionViewSet as BaseRolePermissionViewSet
+
+
+class SmartRoleObjectPermissionSerializer(PassiveSerializer):
+    id = IntegerField(source="pk", read_only=True)
+
+    app_label = CharField(source="content_type__app_label")
+    app_label_verbose = CharField(source="content_type__app_label")
+    model = CharField(source="content_type__model")
+    model_verbose = CharField(source="content_type__model")
+    object_description = SerializerMethodField()
+    codename = CharField(source="permission__codename")
+    name = SerializerMethodField()
+    object_pk = CharField()
+
+    def get_name(self, instance) -> str:
+        app_label = instance["content_type__app_label"]
+        model = instance["content_type__model"]
+        codename = instance["permission__codename"]
+        object_pk = instance["object_pk"]
+        return f"{app_label}.{codename} on {model}#{object_pk}"
+
+    def get_object_description(self, instance) -> str:
+        model = instance["content_type__model"]
+        object_pk = instance["object_pk"]
+
+        return f"{model}#{object_pk}"
+
+
+class RolePermissionViewSet(BaseRolePermissionViewSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.use_suse_queryset_and_serializer = settings.OVERRIDE_ENDPOINT.get(
+            "rbac_permissions_roles_list"
+        )
+
+    def get_serializer_class(self):
+        if not self.use_suse_queryset_and_serializer:
+            return super().get_serializer_class()
+        return SmartRoleObjectPermissionSerializer
+
+    @property
+    def paginator(self):
+        if not self.use_suse_queryset_and_serializer:
+            return super().paginator
+
+        if not hasattr(self, "_paginator"):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                self._paginator = Pagination()
+        return self._paginator
+
+    def get_queryset(self):
+        if not self.use_suse_queryset_and_serializer:
+            return super().get_queryset()
+
+        required_fields = [
+            "pk",
+            "permission__codename",
+            "content_type__app_label",
+            "content_type__model",
+            "object_pk",
+        ]
+        return RoleObjectPermission.objects.all().values(*required_fields)
---
+++ authentik/suse/tests/test_rbac_role_permission_list.py
@@ -0,0 +1,108 @@
+"""Test RolePermissionViewSet api"""
+
+from django.test.utils import override_settings
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from authentik.api.pagination import SmallerPagination
+from authentik.core.models import Group
+from authentik.core.tests.utils import create_test_admin_user, create_test_user
+from authentik.lib.generators import generate_id
+from authentik.rbac.models import Role
+from authentik.stages.invitation.models import Invitation
+from authentik.tenants.models import Tenant
+
+
+class TestRBACPermissionRoles(APITestCase):
+    """Test RolePermissionViewSet api"""
+
+    def setUp(self) -> None:
+        self.superuser = create_test_admin_user()
+
+        self.user = create_test_user()
+        self.role = Role.objects.create(name=generate_id())
+        self.group = Group.objects.create(name=generate_id())
+        self.group.roles.add(self.role)
+        self.group.users.add(self.user)
+
+        self.client.force_login(self.superuser)
+        inv = Invitation.objects.create(
+            name=generate_id(),
+            created_by=self.superuser,
+        )
+        self.role.assign_perms("authentik_stages_invitation.view_invitation", obj=inv)
+
+    def test_original_list(self):
+        with self.assertNumQueries(16):
+            res = self.client.get(reverse("authentik_api:permissions-roles-list"))
+            self.assertEqual(res.status_code, 200)
+
+        for _ in range(1, 40):
+            inv = Invitation.objects.create(
+                name=generate_id(),
+                created_by=self.superuser,
+            )
+            self.role.assign_perms("authentik_stages_invitation.view_invitation", obj=inv)
+
+        with self.assertNumQueries(54):
+            res = self.client.get(reverse("authentik_api:permissions-roles-list"))
+            self.assertEqual(res.status_code, 200)
+            returned_results = len(res.json()["results"])
+            assert (
+                returned_results == Tenant.pagination_default_page_size.field.default
+            ), "Did not obey old pagination"
+
+        # when page_size is provided... it clamps against max... but when not...
+        # max is not taken into account...
+        with self.assertNumQueries(34):
+            res = self.client.get(
+                reverse("authentik_api:permissions-roles-list", query=dict(page_size=20))
+            )
+            self.assertEqual(res.status_code, 200)
+            returned_results = len(res.json()["results"])
+            assert (
+                returned_results == SmallerPagination.max_page_size
+            ), "Did not obey old pagination"
+
+    @override_settings(OVERRIDE_ENDPOINT=dict(rbac_permissions_list=True))
+    def test_improved_list(self):
+        with self.assertNumQueries(14):
+            res = self.client.get(
+                reverse("authentik_api:permissions-roles-list", query=dict(suse_serializer="yes"))
+            )
+            self.assertEqual(res.status_code, 200)
+
+    @override_settings(OVERRIDE_ENDPOINT=dict(rbac_permissions_list=True))
+    def test_improved_list_regular_pagination(self):
+        total_invitations = 1
+        for _ in range(1, 40):
+            inv = Invitation.objects.create(
+                name=generate_id(),
+                created_by=self.superuser,
+            )
+            self.role.assign_perms("authentik_stages_invitation.view_invitation", obj=inv)
+            total_invitations += 1
+
+        with self.assertNumQueries(14):
+            res = self.client.get(
+                reverse("authentik_api:permissions-roles-list", query=dict(suse_serializer="yes"))
+            )
+            self.assertEqual(res.status_code, 200)
+
+            results_count = len(res.json()["results"])
+            assert (
+                results_count == Tenant.pagination_default_page_size.field.default
+            ), "Did not obey new pagination"
+
+        # when page_size is provided... it clamps against tenant max instead.
+        with self.assertNumQueries(14):
+            res = self.client.get(
+                reverse(
+                    "authentik_api:permissions-roles-list",
+                    query=dict(suse_serializer="yes", page_size=100),
+                )
+            )
+            self.assertEqual(res.status_code, 200)
+
+            results_count = len(res.json()["results"])
+            assert results_count == total_invitations, "Did not obey new pagination"
